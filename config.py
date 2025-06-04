7ea2bd3ae9189b1c7ee059014e3b3291787b5229
rajs2695 <rajeshrakis2695@gmail.com>
2025-06-04 16:45:46 +0530

    config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "8045459")) #optional
 API_HASH = getenv("API_HASH", "e6d1f09120e17a4372fe022dde88511b") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8187361583 1281282633").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1281282633 8187361583 7888151947 7948585276").split()))
 OWNER_ID = int(getenv("OWNER_ID", "1281282633"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "7068876137:AAHxyBy6P_UBAL-5eS9S6YBIGAlSyWJ03HA")
@@ -24,7 +24,7 @@ BRANCH = getenv("BRANCH", "main") #don't change
 
 # Sophia
 OWNER_ID = [1281282633] #RAUSHAN.me.id
-SUDO_USERS_ID = [8071602126]
+SUDO_USERS_ID = [8187361583]
 BOTS_ALLOWED_TO_WORK_IN_BUSY_COMMANDS = False
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "BQB6w5MAPCnQr_8FstvmjZyBBPo4EW7vQLdNQU2vgiWbL8t-vE74iiDVLEVMnIbtctM7sfp4QDa4vpV03WqvbehCyJeoxOtmv58KcDG214dQZ81iAWclihGUHB3b-mjSsTVwarUOzM70DMDyGWPlu2odFxYELBQUVr_Go9hRcLeaYg-0Q8ldL6KiIxpQMHjlnwHefLxPvm1Z_jXNfuI0zuOK6R81YSTqaqIOOavO4e6ntZsYxDc39k4cOxZCUP1-Ijo6x_tSYptSq-RWXhvUfJ6wCR_XI7HDz3dcAZQ8IxeWiguW5VgG572JZyjy30z62pCgUl48EAWNC9l8UL7HSG5pUT3RCwAAAABMXtJJAA")
